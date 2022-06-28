c813001601329e4943749446614eebd4e0e2a2b8
Jean1804 <jeanlucianourena@gmail.com>
2022-06-27 22:55:25 -0400

    Save

---
+++ Assets/Scripts/Bola.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour {
+
+    public float velocidad = 30.0f;
+
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * velocidad;
+	}
+
+    private void OnCollisionEnter2D(Collision2D micolision)
+    {
+        if(micolision.gameObject.name == "Raqueta Izquierda")
+        {
+            int x = 1;
+
+            int y = direccionY(transform.position, micolision.transform.position);
+
+            Vector2 direccion = new Vector2(x, y);
+
+            GetComponent<Rigidbody2D>().velocity = direccion * velocidad;
+        }
+
+        if(micolision.gameObject.name == "Raqueta Derecha")
+        {
+            int x = -1;
+
+            int y = direccionY(transform.position, micolision.transform.position);
+
+            Vector2 direccion = new Vector2(x, y);
+
+            GetComponent<Rigidbody2D>().velocity = direccion * velocidad;
+        }
+
+    }
+
+    int direccionY(Vector2 posicionBola, Vector2 posicionRaqueta)
+    {
+        if(posicionBola.y > posicionRaqueta.y)
+        {
+            return 1;
+        }
+        else if(posicionBola.y < posicionRaqueta.y)
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Raqueta.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raqueta : MonoBehaviour {
+
+    public float velocidad = 30.0f;
+    public string eje;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float v = Input.GetAxisRaw(eje);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v * velocidad);
+	}
+}
